671dd44c8b5ba86a7ce540a9b87e434f4e797cc6
Pieter <pieter@pieterdepauw.com>
2024-11-13 08:14:54 +0100

    Rename tables and columns for improved clarity and consistency in database schema

--- src/db/migrations/0001_ordinary_tenebrous.sql
+++ src/db/migrations/0001_ordinary_tenebrous.sql
@@ -1,6 +1,6 @@
-CREATE TABLE IF NOT EXISTS "page_sections" (
+CREATE TABLE IF NOT EXISTS "document_sections" (
 	"id" varchar PRIMARY KEY NOT NULL,
-	"page_id" varchar NOT NULL,
+	"document_id" varchar NOT NULL,
 	"slug" text NOT NULL,
 	"heading" text NOT NULL,
 	"content" text NOT NULL,
@@ -8,29 +8,29 @@ CREATE TABLE IF NOT EXISTS "page_sections" (
 	"embedding" vector(1536) NOT NULL
 );
 --> statement-breakpoint
-CREATE TABLE IF NOT EXISTS "pages" (
+CREATE TABLE IF NOT EXISTS "documents" (
 	"id" varchar PRIMARY KEY NOT NULL,
 	"path" text NOT NULL,
 	"checksum" text,
 	"type" text,
 	"source" text,
 	"meta" text,
-	"parent_page_path" text,
+	"parent_document_path" text,
 	"version" varchar,
 	"last_refresh" timestamp DEFAULT now(),
 	"created_at" timestamp DEFAULT now() NOT NULL,
 	"updated_at" timestamp NOT NULL,
-	CONSTRAINT "pages_path_unique" UNIQUE("path")
+	CONSTRAINT "documents_path_unique" UNIQUE("path")
 );
 --> statement-breakpoint
 DO $$ BEGIN
- ALTER TABLE "page_sections" ADD CONSTRAINT "page_sections_page_id_pages_id_fk" FOREIGN KEY ("page_id") REFERENCES "public"."pages"("id") ON DELETE no action ON UPDATE no action;
+ ALTER TABLE "document_sections" ADD CONSTRAINT "document_sections_document_id_documents_id_fk" FOREIGN KEY ("document_id") REFERENCES "public"."documents"("id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
 --> statement-breakpoint
 DO $$ BEGIN
- ALTER TABLE "pages" ADD CONSTRAINT "pages_parent_page_path_pages_path_fk" FOREIGN KEY ("parent_page_path") REFERENCES "public"."pages"("path") ON DELETE no action ON UPDATE no action;
+ ALTER TABLE "documents" ADD CONSTRAINT "documents_parent_document_path_documents_path_fk" FOREIGN KEY ("parent_document_path") REFERENCES "public"."documents"("path") ON DELETE no action ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
